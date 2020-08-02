4f000b28f6a0ab085a256fb3b357570ae6d1607d
YashinDV73 <69110075+YashinDV73@users.noreply.github.com>
2020-08-02 15:56:13 +0300

    Add files via upload

---
+++ src/collection/map.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class map {
+
+    private static final Map<String,String> STRING_STRING_MAP = new HashMap<>(2);
+
+    public static void main(String[] args) {
+        STRING_STRING_MAP.put("k1" , "v1");
+        STRING_STRING_MAP.put("k2" , "v2");
+        STRING_STRING_MAP.put("k3" , "v2");
+
+        System.out.println(STRING_STRING_MAP);
+    }
+
+}
---
+++ src/regex/RegEx.java
@@ -0,0 +1,35 @@
+package regex;
+
+public class RegEx {
+
+    private static final String matcherAnyOneDigital = "\\d+";
+    private static final String matcherAnyEmptyDigital = "\\d?";
+    private static final String matcherFourDigital = "\\d{4}";
+
+    public static void main(String[] args) {
+
+        testDigitals(matcherAnyOneDigital);
+
+        testDigitals(matcherAnyEmptyDigital);
+
+        testDigitals(matcherFourDigital);
+
+
+    }
+
+    private static void testDigitals(String matcher){
+        String simple1 = "159545";
+        String simple2 = "15e9545";
+        String simple3 = "15954.5";
+        String simple4 = "4423";
+        String simple5 = "";
+
+        System.out.println(simple1.matches(matcher));
+        System.out.println(simple2.matches(matcher));
+        System.out.println(simple3.matches(matcher));
+        System.out.println(simple4.matches(matcher));
+        System.out.println(simple5.matches(matcher));
+        System.out.println("\r");
+    }
+
+}
